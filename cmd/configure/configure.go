896a3d4c28c1da4d7f5644cbf57f189980f8bcb7
Deepak Jois <deepak.jois@gmail.com>
2024-07-04 17:25:00 +0530

    Use huh for text inputs in configure command

--- cmd/configure/configure.go
+++ cmd/configure/configure.go
@@ -2,86 +2,63 @@ package configure
 
 import (
 	"fmt"
+	"strings"
 
-	"github.com/charmbracelet/bubbles/textinput"
-	tea "github.com/charmbracelet/bubbletea"
-	"github.com/charmbracelet/lipgloss"
+	"github.com/charmbracelet/huh"
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
 )
-
-type model struct {
-	textInput textinput.Model
-	err       error
-}
-
-func initialModel() model {
-	ti := textinput.New()
-	ti.Placeholder = "Enter your OpenAI API Key"
-	ti.Focus()
-
-	return model{
-		textInput: ti,
-		err:       nil,
-	}
-}
-
-func (m model) Init() tea.Cmd {
-	return textinput.Blink
-}
-
-func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
-	var cmd tea.Cmd
-
-	switch msg := msg.(type) {
-	case tea.KeyMsg:
-		switch msg.Type {
-		case tea.KeyEnter:
-
-			viper.Set("openai_api_key", m.textInput.Value())
-			err := viper.WriteConfigAs(viper.ConfigFileUsed())
-			if err != nil {
-				m.err = fmt.Errorf("error writing config: %v", err)
-			}
-			return m, tea.Quit
-		case tea.KeyCtrlC, tea.KeyEsc:
-			return m, tea.Quit
-		}
-
-	case error:
-		m.err = msg
-		return m, nil
-	}
-
-	m.textInput, cmd = m.textInput.Update(msg)
-	return m, cmd
-}
-
-func (m model) View() string {
-	if m.err != nil {
-		return fmt.Sprintf("Error: %v\n", m.err)
-	}
-
-	style := lipgloss.NewStyle().
-		Bold(true).
-		Foreground(lipgloss.Color("#FAFAFA")).
-		Background(lipgloss.Color("#7D56F4")).
-		PaddingTop(1).
-		PaddingBottom(1).
-		PaddingLeft(4).
-		PaddingRight(4)
-
-	return style.Render("Enter your OpenAI API Key:") + "\n\n" +
-		m.textInput.View() + "\n\n" +
-		"(press enter to submit)"
-}
 
 var Command = &cobra.Command{
 	Use:   "configure",
 	Short: "Configure podscript with API keys",
 	RunE: func(cmd *cobra.Command, args []string) error {
-		p := tea.NewProgram(initialModel())
-		_, err := p.Run()
-		return err
+
+		// OpenAPI
+		var openAIApiKey string
+		textInput := huh.NewInput().
+			Title("OpenAI API key").
+			Prompt("> ").
+			Placeholder("press Enter to skip or leave unchanged").
+			EchoMode(huh.EchoModePassword).
+			Value(&openAIApiKey)
+		err := textInput.Run()
+		if err != nil && err != huh.ErrUserAborted {
+			return err
+		}
+		openAIApiKey = strings.TrimSpace(openAIApiKey)
+		if openAIApiKey != "" {
+			viper.Set("openai_api_key", openAIApiKey)
+			fmt.Println("OpenAI API key set")
+		} else {
+			fmt.Println("skipping OpenAI API key")
+		}
+
+		// Deepgram
+		var deepgramApiKey string
+		textInput = huh.NewInput().
+			Title("Deepgram API key").
+			Prompt("> ").
+			Placeholder("press Enter to skip or leave unchanged").
+			EchoMode(huh.EchoModePassword).
+			Value(&deepgramApiKey)
+		err = textInput.Run()
+		if err != nil && err != huh.ErrUserAborted {
+			return err
+		}
+		deepgramApiKey = strings.TrimSpace(deepgramApiKey)
+		if deepgramApiKey != "" {
+			viper.Set("deepgram_api_key", deepgramApiKey)
+			fmt.Println("Deepgram API Key set")
+		} else {
+			fmt.Println("skipping Deepgram API key")
+		}
+		err = viper.WriteConfigAs(viper.ConfigFileUsed())
+		if err != nil {
+			return fmt.Errorf("error writing config: %v", err)
+		} else {
+			fmt.Printf("Wrote to %s\n", viper.ConfigFileUsed())
+		}
+		return nil
 	},
 }
--- go.mod
+++ go.mod
@@ -3,17 +3,21 @@ module github.com/deepakjois/podscript
 go 1.22.4
 
 require (
-	github.com/charmbracelet/bubbles v0.18.0
-	github.com/charmbracelet/bubbletea v0.26.6
-	github.com/charmbracelet/lipgloss v0.11.0
+	github.com/charmbracelet/huh v0.4.2
 	github.com/spf13/cobra v1.8.1
 	github.com/spf13/viper v1.19.0
 )
 
 require (
 	github.com/bitly/go-simplejson v0.5.1 // indirect
+	github.com/catppuccin/go v0.2.0 // indirect
+	github.com/charmbracelet/bubbles v0.18.0 // indirect
+	github.com/charmbracelet/bubbletea v0.26.6 // indirect
+	github.com/charmbracelet/lipgloss v0.11.0 // indirect
+	github.com/charmbracelet/x/exp/strings v0.0.0-20240524151031-ff83003bf67a // indirect
 	github.com/dlclark/regexp2 v1.11.0 // indirect
 	github.com/dop251/goja v0.0.0-20240220182346-e401ed450204 // indirect
+	github.com/dustin/go-humanize v1.0.1 // indirect
 	github.com/go-sourcemap/sourcemap v2.1.3+incompatible // indirect
 	github.com/google/pprof v0.0.0-20240227163752-401108e1b7e7 // indirect
 )
@@ -23,9 +27,9 @@ require (
 	github.com/aymanbagabas/go-osc52/v2 v2.0.1 // indirect
 	github.com/cenkalti/backoff/v4 v4.3.0
 	github.com/charmbracelet/x/ansi v0.1.2 // indirect
-	github.com/charmbracelet/x/input v0.1.0 // indirect
+	github.com/charmbracelet/x/input v0.1.1 // indirect
 	github.com/charmbracelet/x/term v0.1.1 // indirect
-	github.com/charmbracelet/x/windows v0.1.0 // indirect
+	github.com/charmbracelet/x/windows v0.1.2 // indirect
 	github.com/erikgeiser/coninput v0.0.0-20211004153227-1c3628e74d0f // indirect
 	github.com/fsnotify/fsnotify v1.7.0 // indirect
 	github.com/hashicorp/hcl v1.0.0 // indirect
@@ -51,12 +55,11 @@ require (
 	github.com/spf13/pflag v1.0.5 // indirect
 	github.com/subosito/gotenv v1.6.0 // indirect
 	github.com/xo/terminfo v0.0.0-20220910002029-abceb7e1c41e // indirect
-	go.uber.org/atomic v1.9.0 // indirect
 	go.uber.org/multierr v1.11.0 // indirect
 	golang.org/x/exp v0.0.0-20240222234643-814bf88cf225 // indirect
 	golang.org/x/sync v0.7.0 // indirect
 	golang.org/x/sys v0.21.0 // indirect
-	golang.org/x/text v0.14.0 // indirect
+	golang.org/x/text v0.15.0 // indirect
 	gopkg.in/ini.v1 v1.67.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
