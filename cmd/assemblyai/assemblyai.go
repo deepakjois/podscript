8219c2e0c9846745140e59c88b845a5f11a1ad34
Swayanshu <swayanshupanda@gmail.com>
2024-10-20 14:20:40 +0530

    add assemblyai subcommand
    Fix #16

---
+++ cmd/assemblyai/assemblyai.go
@@ -0,0 +1,142 @@
+package assemblyai
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"net/url"
+	"os"
+	"path"
+	"path/filepath"
+	"time"
+
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+
+	aai "github.com/AssemblyAI/assemblyai-go-sdk"
+)
+
+const (
+	maxLocalFileSize int64 = 2200 * 1024 * 1024 // Approximate 2.2GB in bytes
+)
+
+func init() {
+	Command.Flags().StringP("path", "p", "", "save transcripts and API responses to path")
+	Command.Flags().StringP("suffix", "s", "", "append suffix to filenames for easier recognition")
+	Command.Flags().BoolP("verbose", "v", false, "fetch verbose JSON response (includes token and start/end timestamps)")
+	Command.Flags().StringP("from-url", "u", "", "URL of the audio file to transcribe")
+	Command.Flags().StringP("from-file", "f", "", "Local path to the audio file to transcribe")
+}
+
+var Command = &cobra.Command{
+	Use:   "assemblyai",
+	Short: "Generate transcript of an audio file using Assembly AI's API.",
+	RunE: func(cmd *cobra.Command, args []string) error {
+		apiKey := viper.GetString("assemblyai_api_key")
+		if apiKey == "" {
+			return errors.New("assembly AI's API key not found. Please run 'podscript configure' or set the ASSEMBLYAI_API_KEY environment variable")
+		}
+
+		folder, _ := cmd.Flags().GetString("path")
+		suffix, _ := cmd.Flags().GetString("suffix")
+		audioURL, _ := cmd.Flags().GetString("from-url")
+		audioFilePath, _ := cmd.Flags().GetString("from-file")
+		verbose, _ := cmd.Flags().GetBool("verbose")
+
+		if folder == "" {
+			folder = "." // Default to current directory if no path is specified
+		}
+
+		folder = filepath.Clean(folder)
+		if fi, err := os.Stat(folder); err != nil || !fi.IsDir() {
+			return fmt.Errorf("path not found: %s", folder)
+		}
+
+		timestamp := time.Now().Format("2006-01-02-150405")
+		filenameSuffix := timestamp
+		if suffix != "" {
+			filenameSuffix = fmt.Sprintf("%s_%s", timestamp, suffix)
+		}
+
+		client := aai.NewClient(apiKey)
+		ctx := context.Background()
+
+		var transcript *aai.Transcript
+		var err error
+
+		if audioURL != "" {
+			// Handle URL input
+			parsedURL, err := url.ParseRequestURI(audioURL)
+			if err != nil || (parsedURL.Scheme != "http" && parsedURL.Scheme != "https") {
+				return fmt.Errorf("invalid URL: %s", audioURL)
+			}
+
+			params := &aai.TranscriptOptionalParams{
+				SpeakerLabels: aai.Bool(true),
+				Punctuate:     aai.Bool(true),
+				FormatText:    aai.Bool(true),
+			}
+			transcriptValue, err := client.Transcripts.TranscribeFromURL(ctx, audioURL, params)
+			if err != nil {
+				return fmt.Errorf("failed to transcribe from URL: %w", err)
+			}
+			transcript = &transcriptValue
+			fmt.Printf("Generated transcript from URL %s\n", audioURL)
+
+		} else if audioFilePath != "" {
+			// Handle file input
+			audioFilePath := filepath.Clean(audioFilePath)
+			fi, err := os.Stat(audioFilePath)
+			if err != nil || fi.IsDir() {
+				return fmt.Errorf("invalid audio file: %s", audioFilePath)
+			}
+
+			if fi.Size() > maxLocalFileSize {
+				return fmt.Errorf("file size exceeds 2.2GB limit")
+			}
+
+			file, err := os.Open(audioFilePath)
+			if err != nil {
+				return fmt.Errorf("error opening file: %w", err)
+			}
+			defer file.Close()
+
+			transcriptValue, err := client.Transcripts.TranscribeFromReader(ctx, file, nil)
+			if err != nil {
+				return fmt.Errorf("failed to transcribe from file: %w", err)
+			}
+			transcript = &transcriptValue
+		} else {
+			return errors.New("please provide either a valid URL or a file path")
+		}
+
+		if transcript == nil || transcript.Text == nil {
+			return errors.New("transcription failed: received nil transcript from AssemblyAI API")
+		}
+
+		transcriptFilename := path.Join(folder, fmt.Sprintf("assemblyai_transcript_%s.txt", filenameSuffix))
+		transcriptFilename = filepath.Clean(transcriptFilename)
+		file, err := os.Create(transcriptFilename)
+		if err != nil {
+			return fmt.Errorf("failed to create transcript file: %w", err)
+		}
+		defer file.Close()
+
+		for _, utterance := range transcript.Utterances {
+			_, err := fmt.Fprintf(file, "Speaker %s: %s\n\n",
+				aai.ToString(utterance.Speaker),
+				aai.ToString(utterance.Text),
+			)
+			if err != nil {
+				return fmt.Errorf("failed to write utterance to file: %w", err)
+			}
+		}
+		fmt.Printf("Wrote transcript to %s\n", transcriptFilename)
+
+		if verbose {
+			fmt.Printf("Transcript metadata: %+v\n", transcript)
+		}
+
+		return nil
+	},
+}
--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/deepakjois/podscript
 go 1.22.4
 
 require (
+	github.com/AssemblyAI/assemblyai-go-sdk v1.8.1
 	github.com/charmbracelet/huh v0.4.2
 	github.com/deepakjois/ytt v0.0.0-20240922124700-664221d83d24
 	github.com/deepgram/deepgram-go-sdk v1.3.6
@@ -12,38 +13,31 @@ require (
 )
 
 require (
+	github.com/atotto/clipboard v0.1.4 // indirect
+	github.com/aymanbagabas/go-osc52/v2 v2.0.1 // indirect
+
+	// indirect dependencies
 	github.com/catppuccin/go v0.2.0 // indirect
+	github.com/cenkalti/backoff v2.2.1+incompatible // indirect
 	github.com/charmbracelet/bubbles v0.18.0 // indirect
 	github.com/charmbracelet/bubbletea v0.26.6 // indirect
 	github.com/charmbracelet/lipgloss v0.11.0 // indirect
-	github.com/charmbracelet/x/exp/strings v0.0.0-20240524151031-ff83003bf67a // indirect
-	github.com/dlclark/regexp2 v1.11.0 // indirect
-	github.com/dustin/go-humanize v1.0.1 // indirect
-	github.com/go-logr/logr v1.4.1 // indirect
-	github.com/google/uuid v1.6.0 // indirect
-	github.com/gorilla/schema v1.3.0 // indirect
-	github.com/pkoukk/tiktoken-go v0.1.6 // indirect
-	github.com/rogpeppe/go-internal v1.12.0 // indirect
-	gitlab.com/golang-commonmark/html v0.0.0-20191124015941-a22733972181 // indirect
-	gitlab.com/golang-commonmark/linkify v0.0.0-20191026162114-a0c2df6c8f82 // indirect
-	gitlab.com/golang-commonmark/markdown v0.0.0-20211110145824-bf3e522c626a // indirect
-	gitlab.com/golang-commonmark/mdurl v0.0.0-20191124015652-932350d1cb84 // indirect
-	gitlab.com/golang-commonmark/puny v0.0.0-20191124015043-9f83538fa04f // indirect
-	gopkg.in/check.v1 v1.0.0-20201130134442-10cb98267c6c // indirect
-	k8s.io/klog/v2 v2.110.1 // indirect
-)
-
-require (
-	github.com/atotto/clipboard v0.1.4 // indirect
-	github.com/aymanbagabas/go-osc52/v2 v2.0.1 // indirect
 	github.com/charmbracelet/x/ansi v0.1.2 // indirect
+	github.com/charmbracelet/x/exp/strings v0.0.0-20240524151031-ff83003bf67a // indirect
 	github.com/charmbracelet/x/input v0.1.1 // indirect
 	github.com/charmbracelet/x/term v0.1.1 // indirect
 	github.com/charmbracelet/x/windows v0.1.2 // indirect
+	github.com/dlclark/regexp2 v1.11.0 // indirect
+	github.com/dustin/go-humanize v1.0.1 // indirect
 	github.com/erikgeiser/coninput v0.0.0-20211004153227-1c3628e74d0f // indirect
 	github.com/fsnotify/fsnotify v1.7.0 // indirect
+	github.com/go-logr/logr v1.4.1 // indirect
+	github.com/google/go-querystring v1.1.0 // indirect
+	github.com/google/uuid v1.6.0 // indirect
+	github.com/gorilla/schema v1.4.1 // indirect
 	github.com/hashicorp/hcl v1.0.0 // indirect
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
+	github.com/klauspost/compress v1.17.6 // indirect
 	github.com/lucasb-eyer/go-colorful v1.2.0 // indirect
 	github.com/magiconair/properties v1.8.7 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
@@ -54,7 +48,9 @@ require (
 	github.com/muesli/cancelreader v0.2.2 // indirect
 	github.com/muesli/termenv v0.15.2 // indirect
 	github.com/pelletier/go-toml/v2 v2.2.2 // indirect
+	github.com/pkoukk/tiktoken-go v0.1.6 // indirect
 	github.com/rivo/uniseg v0.4.7 // indirect
+	github.com/rogpeppe/go-internal v1.12.0 // indirect
 	github.com/sagikazarmark/locafero v0.4.0 // indirect
 	github.com/sagikazarmark/slog-shim v0.1.0 // indirect
 	github.com/sourcegraph/conc v0.3.0 // indirect
@@ -63,11 +59,19 @@ require (
 	github.com/spf13/pflag v1.0.5 // indirect
 	github.com/subosito/gotenv v1.6.0 // indirect
 	github.com/xo/terminfo v0.0.0-20220910002029-abceb7e1c41e // indirect
+	gitlab.com/golang-commonmark/html v0.0.0-20191124015941-a22733972181 // indirect
+	gitlab.com/golang-commonmark/linkify v0.0.0-20191026162114-a0c2df6c8f82 // indirect
+	gitlab.com/golang-commonmark/markdown v0.0.0-20211110145824-bf3e522c626a // indirect
+	gitlab.com/golang-commonmark/mdurl v0.0.0-20191124015652-932350d1cb84 // indirect
+	gitlab.com/golang-commonmark/puny v0.0.0-20191124015043-9f83538fa04f // indirect
 	go.uber.org/multierr v1.11.0 // indirect
 	golang.org/x/exp v0.0.0-20240222234643-814bf88cf225 // indirect
 	golang.org/x/sync v0.7.0 // indirect
 	golang.org/x/sys v0.21.0 // indirect
 	golang.org/x/text v0.15.0 // indirect
+	gopkg.in/check.v1 v1.0.0-20201130134442-10cb98267c6c // indirect
 	gopkg.in/ini.v1 v1.67.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
+	k8s.io/klog/v2 v2.110.1 // indirect
+	nhooyr.io/websocket v1.8.7 // indirect
 )
