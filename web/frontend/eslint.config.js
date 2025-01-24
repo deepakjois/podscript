671b589dd811069fac0bbc1b7139f9141e68d435
Deepak Jois <deepak.jois@gmail.com>
2025-01-24 18:45:42 +0530

    Initial commit of frontend code

---
+++ web/frontend/eslint.config.js
@@ -0,0 +1,41 @@
+import js from '@eslint/js'
+import globals from 'globals'
+import reactHooks from 'eslint-plugin-react-hooks'
+import reactRefresh from 'eslint-plugin-react-refresh'
+import react from 'eslint-plugin-react'
+import tseslint from 'typescript-eslint'
+import prettier from 'eslint-plugin-prettier'
+
+export default tseslint.config(
+  { ignores: ['dist'] },
+  {
+    extends: [
+      js.configs.recommended,
+      ...tseslint.configs.recommendedTypeChecked,
+      ...tseslint.configs.stylisticTypeChecked,
+    ],
+    files: ['**/*.{ts,tsx}'],
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.browser,
+      parserOptions: {
+        project: ['./tsconfig.node.json', './tsconfig.app.json'],
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+    settings: { react: { version: '18.3' } },
+    plugins: {
+      react,
+      prettier,
+      'react-hooks': reactHooks,
+      'react-refresh': reactRefresh,
+    },
+    rules: {
+      ...reactHooks.configs.recommended.rules,
+      'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+      ...react.configs.recommended.rules,
+      ...react.configs['jsx-runtime'].rules,
+      ...prettier.configs.recommended.rules,
+    },
+  },
+)
